5c4d4b1de4bcd67a45eefe0c373b3e7e8bb4b546
Polina-Mirgorodska <mirgorodskapolina@gmail.com>
2026-03-11 19:06:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
 let string = '';
+
+for (let i = 0; i < friends.length; i+= 1) {
+  string += friends[i];
+
+  if (i < friends.length -1) {
+    string += ",";
+  }
+}
+
+console.log(string);
+
+const result = friends.join(",");
+console.log(result);
+
+const card = [
+    'Карточка-1',
+    'Карточка-2',
+    'Карточка-3',
+    'Карточка-4',
+    'Карточка-5',
+];
+
